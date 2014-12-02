fc2fc1d5d001f90de51d235bf0a2070e81b8c09a
qiupeng <mrqiupeng@126.com>
2014-12-02 21:37:36 +0800

    Initial Commit

---
+++ POPView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  POPView
+//
+//  Created by qiupeng on 14-12-2.
+//  Copyright (c) 2014年 RainQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ POPView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  POPView
+//
+//  Created by qiupeng on 14-12-2.
+//  Copyright (c) 2014年 RainQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
